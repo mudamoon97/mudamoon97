892b6c97d55ea2083abdedb98c0d437aac4e52b6
mudamoon97 <53881669+mudamoon97@users.noreply.github.com>
2020-03-23 19:48:46 +0100

    java task

---
+++ index.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Java script</title>
+	<script type="text/javascript">
+		
+let myName = 'Lawal Olayinka Mudashiru';
+
+
+let myCourse = ['html','css','javascript','c#'];
+
+
+console.log('My name is ' + myName + ' and my courses are ' + myCourse);
+
+
+for(let i=0; i<=200; i++){
+	if(i % 2 == 0)
+		console.log(i);
+}
+
+	</script>
+</head>
+<body>	
+
+</body>
+</html>
